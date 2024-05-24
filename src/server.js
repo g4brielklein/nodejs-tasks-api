d638ac10b6c6987581475890b88cd86bbae25c0c
Gabriel Klein <gabrielkleinribas@gmail.com>
2024-05-23 23:54:30 -0300

    added `POST` task endpoint

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 
 const tasks = []
 
@@ -11,6 +12,26 @@ const server = http.createServer(async (req, res) => {
       .end(JSON.stringify(tasks))
   }
 
+  if (method === 'POST' && url === '/tasks') {
+    const buffers = []
+    
+    for await (let chunk of req) {
+      buffers.push(chunk)
+    }
+    
+    req.body = JSON.parse(Buffer.concat(buffers))
+
+    const task = {
+      id: randomUUID(),
+      title: req.body.title,
+      description: req.body.description,
+    }
+
+    tasks.push(task)
+
+    return res.writeHead(201).end()
+  }
+
   return res.writeHead(404).end()
 })
 
@@ -18,4 +39,3 @@ const PORT = 3334
 server.listen(PORT, () => {
   console.log("Server's running")
 })
- 
